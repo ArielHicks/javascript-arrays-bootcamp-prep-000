93625947a3ccd7385f687090fa4d6ad0ea6abd79
Ariel Hicks <hicks.ariel@gmail.com>
2018-12-09 21:41:35 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array, element){
+  var newArray = [element, ...array]
+  return newArray
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  var newArray.unshift = [element, array]
+  return
+}
